cc8a0d8aff79e0af32ebb3580f683dea1202369e
janiii30 <janirapua03@gmail.com>
2026-02-15 20:14:28 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python populate_catalog.py
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='alumnodb').exists() or User.objects.create_superuser('alumnodb', 'alumnodb@example.com', 'alumnodb')"

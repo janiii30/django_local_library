11b72c118c0bde2c94d62c83ed383738470d8f1f
jpc03 <janirapua03@gmail.com>
2026-02-16 11:21:35 +0100

    Create createsu command

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ python manage.py migrate
 
 python populate_catalog.py
 
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='alumnodb').exists() or User.objects.create_superuser('alumnodb', 'alumnodb@example.com', 'alumnodb')"
+python manage.py createsu 
